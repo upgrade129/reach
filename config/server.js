12bf25d11be506907a9cc545018205d38a3e2bf6
upgrade129 <premsanthosh129@outlook.com>
2020-09-15 11:35:28 +0530

    latest commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6247f5b551050db4ce75995943245177'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bf667356-202c-4159-be36-1cdbd9401a53'
+};
